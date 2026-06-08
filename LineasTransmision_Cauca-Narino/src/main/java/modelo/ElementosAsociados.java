4e03016a95842a736b904abc89694f38f99ada6f
SantiagoAriasR22 <santiarojs@gmail.com>
2026-06-07 22:33:29 -0500

    Subo la definicion de clases, otra vez

--- LineasTransmision_Cauca-Narino/src/main/java/modelo/ElementosAsociados.java
+++ LineasTransmision_Cauca-Narino/src/main/java/modelo/ElementosAsociados.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
-/**
- *
- * @author santiago
- */
 public class ElementosAsociados {
     
+    private String segmentoDeBarras;
+    private String bahias;
+    private String lineas;
+    private String transformadores;
+    private String reactores;
+    private String condensadores;
+    private String SVC_STATCOM_FACT;
+    private String asociadoSupervision;
+    
+    public ElementosAsociados(String segmentoDeBarras, String bahias, String lineas, String transformadores, String reactores, String condensadores, String SVC_STATCOM_FACT, String asociadoSupervision){
+        this.segmentoDeBarras=segmentoDeBarras;
+        this.bahias=bahias;
+        this.lineas=lineas;
+        this.transformadores=transformadores;
+        this.reactores=reactores;
+        this.condensadores=condensadores;
+        this.SVC_STATCOM_FACT=SVC_STATCOM_FACT;
+        this.asociadoSupervision=asociadoSupervision;
+    }
+    
+    //getters
+    public String getSegmentoDeBarras(){ return segmentoDeBarras; }
+    public String getBahias(){ return bahias; }
+    public String getLineas(){ return lineas; }
+    public String getTransformadores(){ return transformadores; }
+    public String getReactores(){ return reactores; }
+    public String getCondensadores(){ return condensadores; }
+    public String getSVC_STATCOM_FACT(){ return SVC_STATCOM_FACT; }
+    public String getAsociadoSupersvision(){ return asociadoSupervision; }
+    
+    //setters
+    public void setSegmentoDeBarras(String segmentoDeBarras){ this.segmentoDeBarras=segmentoDeBarras; }
+    public void setBahias(String bahias){ this.bahias=bahias; }
+    public void setLineas(String lineas){ this.lineas=lineas; }
+    public void setTransformadores(String transformadores){ this.transformadores=transformadores; }
+    public void setReactores(String reactores){ this.reactores=reactores; }
+    public void setCondensadores(String condensadores){ this.condensadores=condensadores; }
+    public void setSVC_STATCOM_FACT(String SVC_STATCOM_FACT){ this.SVC_STATCOM_FACT=SVC_STATCOM_FACT; }
+    public void setAsociadoSupervision(String asociadoSupervision){ this.asociadoSupervision=asociadoSupervision; }
 }
